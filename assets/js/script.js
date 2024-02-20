0797752d96072020196e02b1525cf4d3049dd87a
Mahdi <muhammadmahdiulislam13@gmail.com>
2024-02-20 19:08:47 +0600

    third

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,3 +58,15 @@ document.addEventListener("DOMContentLoaded", function () {
     splide.mount();
   });
 });
+
+
+//Funnels Code
+// var gd = document.getElementById('myFunnel');
+// var data = [{type: 'funnelarea', scalegroup: "first", values: [500, 450, 340, 230, 220, 110],
+//     textinfo: "value", title: {position: "top left",},
+//     domain: {x: [0, 0.5], y: [0, 0.5]}}];
+
+// var layout = {width: 600,shapes: [
+//             {x0: 0, x1: 0, y0: 0, y1: 0}]}
+
+// Plotly.newPlot('myFunnel', data, layout);
